6ac1f7204993a601531b41a3f4e8b0d77a0ed759
Gajanan Shivaji Borkar <borkargajanan62@gmail.com>
2025-01-08 19:47:27 +0530

    Add files via upload
    Remaining programs in previous Reprository are uploded here.
    SimpleC, C++ and Java for Learning the Basic Things.
    #ProgrammingSekho...

---
+++ Practice287.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Practice287
+{
+    public static void int main(String Arg[])
+    
+    {
+        public:
+
+        int Std = 0;
+
+        if(std == 6)
+        System.out.println("Your Wake up time ");
+
+        
+        if(std == 7)
+        System.out.println("Your Brush time ");
+        
+        if(std == 8)
+        System.out.println("Your Toilet time ");
+        
+        if(std == 9)
+        System.out.println("Your Study time ");
+        
+        if(std == 10)
+        System.out.println("Your Break Fast time ");
+
+        else
+        System.out.println(" YOU SLEEP");
+
+
+    }
+}
